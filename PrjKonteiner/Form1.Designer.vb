82f83ba2298ba86bbeb8bb839866da7d005ca788
krmelu <krmelu@ICT-50124K>
2024-02-07 14:53:36 +0200

    buttons, forum, timer on tehtud

--- PrjKonteiner/Form1.Designer.vb
+++ PrjKonteiner/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class formKasutajaAken
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,138 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtSisendTekst = New System.Windows.Forms.TextBox()
+        Me.SisendTekst = New System.Windows.Forms.Label()
+        Me.txtValjundTekst1 = New System.Windows.Forms.TextBox()
+        Me.valjund = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtValjundTekst2 = New System.Windows.Forms.TextBox()
+        Me.btnPoora1 = New System.Windows.Forms.Button()
+        Me.btnPoora2 = New System.Windows.Forms.Button()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.timerUuenda = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'txtSisendTekst
+        '
+        Me.txtSisendTekst.Location = New System.Drawing.Point(228, 136)
+        Me.txtSisendTekst.Name = "txtSisendTekst"
+        Me.txtSisendTekst.Size = New System.Drawing.Size(194, 26)
+        Me.txtSisendTekst.TabIndex = 0
+        '
+        'SisendTekst
+        '
+        Me.SisendTekst.AutoSize = True
+        Me.SisendTekst.Location = New System.Drawing.Point(108, 136)
+        Me.SisendTekst.Name = "SisendTekst"
+        Me.SisendTekst.Size = New System.Drawing.Size(93, 20)
+        Me.SisendTekst.TabIndex = 1
+        Me.SisendTekst.Text = "Sisendtekst"
+        '
+        'txtValjundTekst1
+        '
+        Me.txtValjundTekst1.Location = New System.Drawing.Point(228, 218)
+        Me.txtValjundTekst1.Name = "txtValjundTekst1"
+        Me.txtValjundTekst1.Size = New System.Drawing.Size(194, 26)
+        Me.txtValjundTekst1.TabIndex = 2
+        '
+        'valjund
+        '
+        Me.valjund.AutoSize = True
+        Me.valjund.Location = New System.Drawing.Point(17, 221)
+        Me.valjund.Name = "valjund"
+        Me.valjund.Size = New System.Drawing.Size(184, 20)
+        Me.valjund.TabIndex = 3
+        Me.valjund.Text = "Pööramine funktsiooniga"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(26, 271)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(175, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Pöörame protseduuriga"
+        '
+        'txtValjundTekst2
+        '
+        Me.txtValjundTekst2.Location = New System.Drawing.Point(228, 265)
+        Me.txtValjundTekst2.Name = "txtValjundTekst2"
+        Me.txtValjundTekst2.Size = New System.Drawing.Size(194, 26)
+        Me.txtValjundTekst2.TabIndex = 5
+        '
+        'btnPoora1
+        '
+        Me.btnPoora1.Location = New System.Drawing.Point(175, 174)
+        Me.btnPoora1.Name = "btnPoora1"
+        Me.btnPoora1.Size = New System.Drawing.Size(300, 38)
+        Me.btnPoora1.TabIndex = 6
+        Me.btnPoora1.Text = "Pööra funktsiooniga"
+        Me.btnPoora1.UseVisualStyleBackColor = True
+        '
+        'btnPoora2
+        '
+        Me.btnPoora2.Location = New System.Drawing.Point(217, 297)
+        Me.btnPoora2.Name = "btnPoora2"
+        Me.btnPoora2.Size = New System.Drawing.Size(226, 33)
+        Me.btnPoora2.TabIndex = 7
+        Me.btnPoora2.Text = "Pööra protseduuriga"
+        Me.btnPoora2.UseVisualStyleBackColor = True
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(103, 365)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(226, 33)
+        Me.btnStart.TabIndex = 8
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(348, 365)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(226, 33)
+        Me.btnStop.TabIndex = 9
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'timerUuenda
+        '
+        '
+        'formKasutajaAken
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(715, 524)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.btnPoora2)
+        Me.Controls.Add(Me.btnPoora1)
+        Me.Controls.Add(Me.txtValjundTekst2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.valjund)
+        Me.Controls.Add(Me.txtValjundTekst1)
+        Me.Controls.Add(Me.SisendTekst)
+        Me.Controls.Add(Me.txtSisendTekst)
+        Me.Name = "formKasutajaAken"
+        Me.Text = "formKasutajaAken"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txtSisendTekst As TextBox
+    Friend WithEvents SisendTekst As Label
+    Friend WithEvents txtValjundTekst1 As TextBox
+    Friend WithEvents valjund As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtValjundTekst2 As TextBox
+    Friend WithEvents btnPoora1 As Button
+    Friend WithEvents btnPoora2 As Button
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents timerUuenda As Timer
 End Class
