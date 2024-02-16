53c1d962055763a61f087334dd78b9bff85a977a
Kristina <Julia@DESKTOP-V1SKVB5>
2024-02-16 17:55:01 +0200

    Juhend 1 Fin versioon

--- PrjKonteiner/formKasutajaAken.vb
+++ PrjKonteiner/formKasutajaAken.vb
@@ -1,13 +1,13 @@
 ﻿Imports System.Runtime.Remoting.Contexts
-Imports PrjTekstiPooraja1
+Imports PrjTekstiPooraja2
 
-Public Class Form1
+Public Class formKasutajaAken
     Private Sub btnPoora1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) _
         Handles btnPoora1.Click
         'Objekti loomine liidese baasil; referentsmuutuja pooraja deklatsatsioon
-        Dim Pooraja As PrjTekstiPooraja1.ITeisendused
+        Dim Pooraja As PrjTekstiPooraja2.ITeisendused
         'Seadistame referentsmuutuja viitama uuele objektile
-        Pooraja = New PrjTekstiPooraja1.CTekstiPooraja
+        Pooraja = New PrjTekstiPooraja2.CTekstiPooraja
         'TEOSTAME TEKSTI PÖÖRAMISE KASUTADES PARAMEETRISEERITA FUNKTSIOONI pooraTekst 
         'NING LIIDESE ATRIBUUTI strTekst
         Pooraja.strTekst = txtSisendTekst.Text
@@ -17,13 +17,13 @@ Public Class Form1
     Private Sub btnPoora2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) _
         Handles btnPoora2.Click
         'Objekti loomine liidese baasil; referentsmuutuja pooraja deklatsatsioon
-        Dim Pooraja As PrjTekstiPooraja1.ITeisendused
+        Dim Pooraja As PrjTekstiPooraja2.ITeisendused
         'Seadistame referentsmuutuja viitama uuele objektile
-        Pooraja = New PrjTekstiPooraja1.CTekstiPooraja
+        Pooraja = New PrjTekstiPooraja2.CTekstiPooraja
         'TEOSTAME TEKSTI PÖÖRAMISE KASUTADES PARAMEETRISEERITA FUNKTSIOONI pooraTekst 
         'NING LIIDESE ATRIBUUTI strTekst
-        Pooraja.strTekst = txtSisendTekst.Text
-        txtValjundTekst2.Text = Pooraja.pooraTekst
+        Pooraja.teisendaTekst(txtSisendTekst.Text)
+        txtValjundTekst2.Text = Pooraja.strTekst
     End Sub
 
     Private Sub btnStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStart.Click
@@ -31,22 +31,23 @@ Public Class Form1
         btnStart.Enabled = False
     End Sub
 
-    Private Sub btnStop_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStop.Click
-        timerUuenda.Enabled = False
-        btnStart.Enabled = True
-    End Sub
-
     Private Sub timerUuenda_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) _
         Handles timerUuenda.Tick
         'Objekti loomine liidese baasil; referentsmuutuja pooraja deklatsatsioon
-        Dim Pooraja As PrjTekstiPooraja1.ITeisendused
+        Dim Pooraja As PrjTekstiPooraja2.ITeisendused
         'Seadistame referentsmuutuja viitama uuele objektile
-        Pooraja = New PrjTekstiPooraja1.CTekstiPooraja
+        Pooraja = New PrjTekstiPooraja2.CTekstiPooraja
         'TEOSTAME TEKSTI PÖÖRAMISE KASUTADES PARAMEETRISEERITA FUNKTSIOONI pooraTekst 
         'NING LIIDESE ATRIBUUTI strTekst
         Pooraja.strTekst = txtSisendTekst.Text
         txtValjundTekst1.Text = Pooraja.pooraTekst
     End Sub
+
+    Private Sub btnStop_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStop.Click
+        timerUuenda.Enabled = False
+        btnStart.Enabled = True
+    End Sub
+
     'Kirjutame funktsioon pikkuse arvu tekstis leidmiseks kasutades funktsioon Len()
     Function charCount(text As String) As Integer
         Return Len(text)
@@ -71,7 +72,7 @@ Public Class Form1
 
     Private Sub CAlgoCheck_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs) _
         Handles CAlgoCheck.CheckedChanged
-        Dim teisendus As PrjTekstiPooraja1.ITeisendused
+        Dim teisendus As PrjTekstiPooraja2.ITeisendused
         If CAlgoCheck.Checked Then
             teisendus = New CAlgoritmilinePooraja
         Else
